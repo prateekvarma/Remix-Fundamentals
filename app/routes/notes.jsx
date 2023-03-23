7e85d4bf9df1413a48db86ba11ff193b0e2abc01
prateekvarma <prtk.varma@gmail.com>
2023-03-23 13:24:57 -0600

    Action function to add a new note through a form

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,13 +1,37 @@
-import NewNote, { links as newNoteLinks } from "../components/NewNote";
+import { redirect } from '@remix-run/node';
+import NewNote, { links as newNoteLinks } from '../components/NewNote';
+import { getStoredNotes, storeNotes } from '../data/notes';
 
 export default function NotesPage() {
-    return (
-      <main>
-        <NewNote />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <NewNote />
+    </main>
+  );
+}
 
-  export function links() {
-    return [...newNoteLinks()];
-  }
+export async function action(data) {
+  const { request } = data;
+  const formData = request.formData();
+
+  // const noteData = {
+  //   title: formData.get('title'),
+  //   content: formData.get('content')
+  // }
+
+  const noteData = Object.fromEntries(formData);
+
+  const existingData = await getStoredNotes();
+
+  noteData.id = new Date().toISOString();
+
+  const updatedNotes = existingData.concat(noteData);
+
+  await storeNotes(updatedNotes);
+
+  return redirect('/notes');
+}
+
+export function links() {
+  return [...newNoteLinks()];
+}

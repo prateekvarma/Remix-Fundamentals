910249873f4c6c582c4b90a6ad7c6fe36389b27c
prateekvarma <prtk.varma@gmail.com>
2023-03-25 15:34:27 -0600

    Using useNavigation to check submitting status and disabling the button

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,20 +1,27 @@
+import { Form, useNavigation } from '@remix-run/react';
 import styles from './NewNote.css';
 
 function NewNote() {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
-    <form method="post" id="note-form">
+    <Form method='post' id='note-form'>
       <p>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" required />
+        <label htmlFor='title'>Title</label>
+        <input type='text' id='title' name='title' required />
       </p>
       <p>
-        <label htmlFor="content">Content</label>
-        <textarea id="content" name="content" rows="5" required />
+        <label htmlFor='content'>Content</label>
+        <textarea id='content' name='content' rows='5' required />
       </p>
-      <div className="form-actions">
-        <button>Add Note</button>
+      <div className='form-actions'>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Adding...' : 'Add Note'}
+        </button>
       </div>
-    </form>
+    </Form>
   );
 }
 

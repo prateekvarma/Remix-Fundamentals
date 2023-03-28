724bcedfce93796683e86d37dd653d2f708b1bec
prateekvarma <prtk.varma@gmail.com>
2023-03-28 15:49:32 -0600

    Defined ErrorBoundary() for root and notes component

--- app/root.jsx
+++ app/root.jsx
@@ -1,24 +1,25 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import MainNavigation from "./components/MainNavigation";
+} from '@remix-run/react';
+import MainNavigation from './components/MainNavigation';
 
-import styles from "./styles/main.css"
+import styles from './styles/main.css';
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
@@ -36,6 +37,31 @@ export default function App() {
   );
 }
 
+export function ErrorBoundary({ error }) {
+  <html lang='en'>
+    <head>
+      <Meta />
+      <Links />
+      <title>An error occurred</title>
+    </head>
+    <body>
+      <header>
+        <MainNavigation />
+      </header>
+      <main className='error'>
+        <h1>An error occurred</h1>
+        <p>{error.message}</p>
+        <p>
+          back to <Link to='/'>home</Link>
+        </p>
+      </main>
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>;
+}
+
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: 'stylesheet', href: styles }];
 }
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import NewNote, { links as newNoteLinks } from '../components/NewNote';
 import NoteList, { links as noteListLinks } from '../components/NoteList';
 import { getStoredNotes, storeNotes } from '../data/notes';
@@ -32,8 +32,8 @@ export async function action(data) {
   const noteData = Object.fromEntries(formData);
 
   //validation
-  if(noteData.title.trim().length < 5) {
-    return { message: 'Title must be at least 5 chars long' }
+  if (noteData.title.trim().length < 5) {
+    return { message: 'Title must be at least 5 chars long' };
   }
 
   const existingData = await getStoredNotes();
@@ -49,4 +49,16 @@ export async function action(data) {
 
 export function links() {
   return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <main className='error'>
+      <h1>Error from notes details page</h1>
+      <p>{error.message}</p>
+      <p>
+        back to <Link to='/'>home</Link>
+      </p>
+    </main>
+  );
 }

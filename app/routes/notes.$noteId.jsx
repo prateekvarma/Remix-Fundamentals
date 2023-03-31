3652b5bf148fef0cddd9e71981f781dcbed84a26
prateekvarma <prtk.varma@gmail.com>
2023-03-31 12:12:17 -0600

    Added meta data to dynamic route

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -39,3 +39,10 @@ export async function loader({ params }) {
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
 }
+
+export function meta({ data }) {
+  return {
+    title: data.title,
+    description: 'Easily manage all your notes',
+  };
+}
